1d3f84cf5409d92847747ed48843ad4deddafc88
Owais <warsimuhammadowais@gmail.com>
2024-04-06 19:44:38 +0530

    updates

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,76 +1,79 @@
 import { useEffect, useState } from 'react';
 import Web3 from 'web3';
-import ABI from '../../abi.json';
+import ABI from '../../blogs.json';
 import { useLocation } from 'react-router-dom';
 
-const contractAddress = "0x8b772cd0f148b3c969fa9e2d2a5fc9c70b1bc958";
+const contractAddress = "0xa8205890dcf006cf801c01f664f2a5a528a27b3c";
 const abi = ABI;
 
 
 export default function Main() {
-  const [storeValue, setStoreValue] = useState("");
-  const [error, setError] = useState(null);
-  const [contract, setContract] = useState(null);
+    const [storeValue, setStoreValue] = useState([]);
+    const [error, setError] = useState(null);
+    const [contract, setContract] = useState(null);
 
-  const state = useLocation();
-  const account = state & state.meProp;
+    const [title, setTitle] = useState();
+    const [body, setBody] = useState()
 
-  const [name,setName] = useState();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        // Check if MetaMask or an Ethereum provider is available
-        if (window.ethereum) {
-          // Initialize Web3 with the current provider
-          const web3 = new Web3(window.ethereum);
 
-          // Request access to the user's MetaMask account
-          await window.ethereum.request({ method: 'eth_requestAccounts' });
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                // Check if MetaMask or an Ethereum provider is available
+                if (window.ethereum) {
+                    // Initialize Web3 with the current provider
+                    const web3 = new Web3(window.ethereum);
 
-          // Create a contract instance
-          const contractInstance = new web3.eth.Contract(abi, contractAddress);
-          setContract(contractInstance);
+                    // Request access to the user's MetaMask account
+                    await window.ethereum.request({ method: 'eth_requestAccounts' });
 
-          // Call the contract function to get the value stored in the 'store' variable
-          const value = await contractInstance.methods.get().call();
-          setStoreValue(value);
-        } else {
-          throw new Error('MetaMask or an Ethereum provider is not detected');
-        }
-      } catch (error) {
-        setError(error.message);
-      }
-    };
+                    // Create a contract instance
+                    const contractInstance = new web3.eth.Contract(abi, contractAddress);
+                    setContract(contractInstance);
 
-    fetchData();
-  }, []);
+                    // Call the contract function to get the value stored in the 'store' variable
+                    const value = await contractInstance.methods.getBlog().call();
+                    setStoreValue(value);
+                } else {
+                    throw new Error('MetaMask or an Ethereum provider is not detected');
+                }
+            } catch (error) {
+                setError(error.message);
+            }
+        };
 
-  const setValue = async () => {
-    try {
-      // Call the contract function to set a new value
-      await contract.methods.set(name).send({ from: window.ethereum.selectedAddress });
+        fetchData();
+    }, []);
 
-      // Update the stored value
-      const newValue = await contract.methods.get().call();
-      setStoreValue(newValue);
-    } catch (error) {
-      setError(error.message);
+    const upload = async () => {
+        await contract.methods.createBlog(title, body).send({ from: window.ethereum.selectedAddress })
     }
-  };
 
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
 
-  return (
-    <div>
-      <h1>Smart Contract Interaction</h1>
-      Current address {account}
-      <br></br>
-      <input type="text" onChange={(e) => setName(e.target.value)}/>
-      <button onClick={setValue}>Set New Value</button>
-      <p>Value stored in the contract: {storeValue}</p>
-    </div>
-  );
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+
+    return (
+        <div>
+            <h1>Smart Contract Interaction</h1>
+            <input type="text" onChange={(e) => setTitle(e.target.value)} />
+            <input type="text" onChange={(e) => setBody(e.target.value)} />
+            <button onClick={upload}>upload Blog</button>
+            <br />
+            {
+                storeValue && storeValue.map((value, index) => (
+                    <ul key={index}>
+                        <li>{value.title}</li>
+                        <li>{value.body}</li>
+                    </ul>
+                ))
+            }
+
+
+        </div>
+    );
 }
+
